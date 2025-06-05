0aea2c6bea55f36380fadcf8c68cfd65b1b7956a
Jay <jaypareekjnp@gmail.com>
2025-06-05 23:15:34 +0530

    book using client details, booking search and list classes with timezone toggling

--- core/serializers.py
+++ core/serializers.py
@@ -1,7 +1,9 @@
+import pytz
 from rest_framework import serializers
 from .models import *
+from django.utils import timezone
 
-# all the model's serializers are created
+# all the model's serializers are created.
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -9,9 +11,44 @@ class UserSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class ClassesSerializer(serializers.ModelSerializer):
+    
+    delivery_date_local = serializers.SerializerMethodField()
+    cutoff_date_local = serializers.SerializerMethodField()
+    server_timezone = serializers.SerializerMethodField()
+    server_time = serializers.SerializerMethodField()
+
     class Meta:
         model = Classes
-        fields = '__all__'
+        fields = '__all__'  
+        read_only_fields = ('server_timezone', 'server_time','delivery_date_local', 'cutoff_date_local')
+
+    def get_timezone(self):
+        request = self.context.get('request')
+        tzname = request.query_params.get('tz') if request else None
+        try:
+            return pytz.timezone(tzname) if tzname else timezone.get_current_timezone()
+        except pytz.UnknownTimeZoneError:
+            return timezone.get_current_timezone()
+
+    def convert_to_user_timezone(self, dt):
+        if not dt:
+            return None
+        user_tz = self.get_timezone()
+        if timezone.is_naive(dt):
+            dt = timezone.make_aware(dt)
+        return dt.astimezone(user_tz).isoformat()
+
+    def get_delivery_date_local(self, obj):
+        return self.convert_to_user_timezone(obj.delivery_date)
+
+    def get_cutoff_date_local(self, obj):
+        return self.convert_to_user_timezone(obj.cutoff_date)
+    
+    def get_server_timezone(self, obj):
+        return str(timezone.get_current_timezone())
+
+    def get_server_time(self, obj):
+        return timezone.now().isoformat()
 
 class ActivitiesSerializer(serializers.ModelSerializer):
     class Meta:
@@ -21,3 +58,9 @@ class BookingsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Bookings
         fields = '__all__'
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Bookings
+        fields = '__all__'
+        read_only_fields = ('is_active','is_waitlisted','created_by', 'updated_by')
--- core/views.py
+++ core/views.py
@@ -3,20 +3,101 @@ from django.contrib.auth.models import User
 from django.core.validators import validate_email
 from rest_framework import viewsets
 from rest_framework import status
+from rest_framework.decorators import action
 from .models import *
 from .serializers import *
 from django.http import JsonResponse
 from rest_framework.response import Response
 from datetime import datetime
 from rest_framework import filters
-
+from rest_framework.exceptions import MethodNotAllowed
+from django.core.exceptions import ValidationError
+import re
 
 # logger
 logger = logging.getLogger(__name__)
 
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UsersList
+def is_valid_email_regex(email):
+    pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+    return re.match(pattern, email) is not None
+
+class ClassesViewSet(viewsets.ModelViewSet):
+    queryset = Classes.objects.all()
+    serializer_class = ClassesSerializer
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context['request'] = self.request
+        return context
     
+class ActivitiesViewSet(viewsets.ModelViewSet):
+    queryset = Activities.objects.all()
+    serializer_class = ActivitiesSerializer
+    
+class BookingsViewSet(viewsets.ModelViewSet):
+    queryset = Bookings.objects.all()
+    serializer_class = BookingsSerializer
+    http_method_names = ['get']
+
+    def list(self, request, *args, **kwargs):
+        email = request.query_params.get('email', None)
+
+        if email:
+            try:
+                validate_email(email)
+            except ValidationError:
+                return Response(
+                    {'error': 'Invalid email address.'},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+
+            bookings = self.queryset.filter(client_email=email)
+            if not bookings.exists():
+                return Response(
+                    {'error': 'No bookings found for the given email.'},
+                    status=status.HTTP_404_NOT_FOUND
+                )
+        else:
+            bookings = self.queryset.all()
+
+        serializer = self.get_serializer(bookings, many=True)
+        return Response(serializer.data)
+
+    def create(self, request, *args, **kwargs):
+        raise MethodNotAllowed("POST")
+
+    def update(self, request, *args, **kwargs):
+        raise MethodNotAllowed("PUT")
+
+    def partial_update(self, request, *args, **kwargs):
+        raise MethodNotAllowed("PATCH")
+
+    def destroy(self, request, *args, **kwargs):
+        raise MethodNotAllowed("DELETE")
 
 
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Classes.objects.all()
+    serializer_class = BookSerializer
+    http_method_names = ['post']
+
+    def perform_create(self, serializer):
+        user = User.objects.filter(is_superuser=True).first()
+        serializer.save(
+            created_by=user,
+            updated_by=user
+        )
+
+    def list(self, request, *args, **kwargs):
+        raise MethodNotAllowed("GET")
+
+    def retrieve(self, request, *args, **kwargs):
+        raise MethodNotAllowed("GET")
+
+    def update(self, request, *args, **kwargs):
+        raise MethodNotAllowed("PUT")
+
+    def partial_update(self, request, *args, **kwargs):
+        raise MethodNotAllowed("PATCH")
+
+    def destroy(self, request, *args, **kwargs):
+        raise MethodNotAllowed("DELETE")
